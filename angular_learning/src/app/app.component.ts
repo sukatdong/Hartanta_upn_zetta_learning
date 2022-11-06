a0fd65f2867de2a9b269621b77909a66d22f8d03
Hartanta Sembiring <hartantasembiring91@gmail.com>
2022-11-06 13:51:46 +0700

    changes

--- angular_learning/src/app/app.component.ts
+++ angular_learning/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  styles:[`[highlight]{background: #f5a95d}`,
+  '[coba]{background: red; color: white}'
+]
 })
 export class AppComponent {
   title = 'angular_learning';
+  @ViewChild('el') bgGreen?:ElementRef;
+  @ViewChild('al') bgRed?:ElementRef;
+
+
+  ngAfterViewInit(){
+    this.bgGreen?.nativeElement.setAttribute('highlight', '');
+    this.bgRed?.nativeElement.setAttribute('coba', '');
+   
+  } 
 }
--- angular_learning/src/app/kasir/menu/menu.component.ts
+++ angular_learning/src/app/kasir/menu/menu.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output,AfterViewInit,ViewChild, ElementRef } from '@angular/core';
 import { item } from '../kasir-big/kasir-big.component';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  styles:['[bgaq]{  background-color: aquamarine;}',
+  '[coba]{background: red; color: white}'
+]
+
 })
 export class MenuComponent implements OnInit {
 
   @Input('items') items: item[]=[]
   @Output ('itemAdded') onAddItem : EventEmitter<item> = new EventEmitter<item>()
-
+  @ViewChild('bgaq') bgaq?:ElementRef;
+  @ViewChild('al') bgRed?:ElementRef;
   constructor() { }
 
   ngOnInit(): void {
@@ -18,5 +23,11 @@ export class MenuComponent implements OnInit {
   addItem(item :item) : void{
     this.onAddItem.emit(item)
   }
+  ngAfterViewInit(){
+
+    this.bgaq?.nativeElement.setAttribute('bgaq', '');
+    this.bgRed?.nativeElement.setAttribute('coba', '');
+   
+  } 
 
 }
