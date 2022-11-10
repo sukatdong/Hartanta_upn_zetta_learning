aa54dbdd814fac8aee063730d1b29d6f8ba3d43e
Hartanta Sembiring <hartantasembiring91@gmail.com>
2022-11-10 13:20:02 +0700

    changes

--- angular_learning/src/app/app-routing.module.ts
+++ angular_learning/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './user-manager/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {
+    path : 'user-managment',
+    loadChildren:()=>import('./user-manager/user-manager.module').then(m=> m.UserManagerModule),
+  },
+
+  {
+    path : '**',
+    redirectTo : 'user-managment'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
